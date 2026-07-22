4fdb1adf1b0f3f25e4152f1325b0e9e486c38a5e
Daniel Avila <daniel.avila@rcac.org>
2026-07-22 13:49:18 -0500

    loaded policy documents into vdb, made collection accessible

--- vector_database.py
+++ vector_database.py
@@ -7,23 +7,22 @@ from sentence_transformers import SentenceTransformer
 
 path = 'C:/Users/daniel.avila/Documents/Github Repos/RCAC/one off projects/RAG_Pipeline/policy repository'
 
-# 1. load documents
 def build_vector_database(path):
     model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
     # first intializde the database
-    # note that this db is not persistent, so will be loaded in memory and then lost when program ends
-    vdb = chromadb.Client()
-    collection = vdb.create_collection(name = "policy_docs")
-
+    # that this db is persistent, so files will be saved to disk
+    vdb = chromadb.PersistentClient(path="./chroma_db")
+    collection = vdb.get_or_create_collection(name = "policy_docs")
 
     nlp = English()
     nlp.add_pipe('sentencizer')
     for doc in os.listdir(path): 
         doc_name = doc
+        # 1. reading text from documents
         doc = nlp(open(os.path.join(path, doc)).read())
 
-        # 2. chunk (using sentence chunking)
+        # 2. break apart to sentences and chunk (using sentence chunking from spacy)
         sentences = [sentence.text.strip() for sentence in doc.sents]
         ids = [f'{doc_name}_{i}' for i in range(len(sentences))]
 
@@ -32,6 +31,9 @@ def build_vector_database(path):
 
         # 4. store in vector database
         collection.add(embeddings = embeddings, 
+                documents = sentences,
                 ids = ids,
                 metadatas = [{'source': doc_name} for _ in sentences])
     return vdb, collection
+
+vdb, collection = build_vector_database(path)
